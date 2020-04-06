612a2f3c11ab5bd6624d4f212daaf3e96bfd3484
Kyle Rathman <krathman257@gmail.com>
2020-04-05 19:04:09 -0600

    Added return functionality

--- Concept.py
+++ Concept.py
@@ -10,3 +10,13 @@ class Concept:
         def addWord(self, w):
                 if self.words.count(w) == 0:
                         self.words.append(w)
+						
+        def getConceptList(self):
+                return self.words
+
+        def getComplexConceptList(self, start, end):
+                tempArr = []
+                for i in range(0, len(self.words)):
+                        tempStr = start + self.words[i] + end
+                        tempArr.append(tempStr)
+                return tempArr
